d4f8e41cdddffe9b6ed33fc08d5c677b85486b78
twi <flutterdash@qq.com>
2022-07-25 23:39:39 +0800

    新功能 简单的计时工具

---
+++ vertlin-support-boot/src/main/kotlin/xyz/scootaloo/vertlin/boot/util/StopWatch.kt
@@ -0,0 +1,43 @@
+package xyz.scootaloo.vertlin.boot.util
+
+import xyz.scootaloo.vertlin.boot.core.currentTimeMillis
+
+/**
+ * @author flutterdash@qq.com
+ * @since 2022/7/25 下午9:47
+ */
+class StopWatch {
+
+    private var taskMapper = HashMap<String, TaskInfo>()
+
+    fun start(name: String = "") {
+        val task = taskMapper[name]
+        if (task != null) {
+            throw IllegalStateException("当前已存在计时任务, 无法重复创建")
+        }
+
+        val newTask = TaskInfo()
+        taskMapper[name] = newTask
+    }
+
+    fun stop(name: String): Long {
+        val task = taskMapper[name] ?: throw IllegalStateException("当前容器内没有名为'$name'的计时任务")
+        if (task.finished) {
+            throw IllegalStateException("该计时任务已经完成, 不能重复标记")
+        }
+        task.timeMills = currentTimeMillis() - task.startTime
+        task.finished = true
+        return task.timeMills
+    }
+
+    fun clear() {
+        taskMapper.clear()
+    }
+
+    private class TaskInfo(
+        var finished: Boolean = false,
+        val startTime: Long = currentTimeMillis(),
+        var timeMills: Long = startTime
+    )
+
+}
