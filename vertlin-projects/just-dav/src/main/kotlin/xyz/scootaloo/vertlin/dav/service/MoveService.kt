adc48196e34d786041854d34d97efc0809cd3705
twi <flutterdash@qq.com>
2022-08-11 15:17:47 +0800

    新功能 move

--- vertlin-projects/just-dav/src/main/kotlin/xyz/scootaloo/vertlin/dav/service/MoveService.kt
+++ vertlin-projects/just-dav/src/main/kotlin/xyz/scootaloo/vertlin/dav/service/MoveService.kt
@@ -1,15 +1,34 @@
 package xyz.scootaloo.vertlin.dav.service
 
 import io.vertx.ext.web.RoutingContext
+import io.vertx.kotlin.core.file.copyOptionsOf
+import io.vertx.kotlin.coroutines.await
+import xyz.scootaloo.vertlin.dav.util.DualFileOperations
 
 /**
  * @author flutterdash@qq.com
  * @since 2022/8/10 下午11:15
  */
-object MoveService {
+object MoveService : DualFileOperations() {
 
     suspend fun move(ctx: RoutingContext) {
+        handle(ctx)
+    }
 
+    private val copyOptional by lazy {
+        copyOptionsOf(atomicMove = false, replaceExisting = true)
+    }
+
+    override fun actionName(): String {
+        return "移动文件"
+    }
+
+    override suspend fun action(from: String, to: String) {
+        fs.move(from, to, copyOptional).await()
+    }
+
+    override suspend fun actionRecursive(from: String, to: String) {
+        fs.move(from, to, copyOptional).await()
     }
 
 }
