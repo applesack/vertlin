16f3bd030f478fe80179a00000de35613ffb1ea7
twi <flutterdash@qq.com>
2022-08-12 08:14:26 +0800

    修复 jar模式下包扫描器失效的问题

--- vertlin-boot/src/main/kotlin/xyz/scootaloo/vertlin/boot/util/PackScanner.kt
+++ vertlin-boot/src/main/kotlin/xyz/scootaloo/vertlin/boot/util/PackScanner.kt
@@ -1,12 +1,9 @@
 package xyz.scootaloo.vertlin.boot.util
 
-import xyz.scootaloo.vertlin.boot.core.ifNotNull
 import java.io.File
-import java.io.IOException
 import java.net.JarURLConnection
 import java.net.URL
 import java.net.URLDecoder
-import java.util.*
 import java.util.jar.JarFile
 
 /**
@@ -16,100 +13,95 @@ import java.util.jar.JarFile
 object PackScanner {
 
     fun scan(pack: String): Set<Class<*>> {
-        val classes = LinkedHashSet<Class<*>>()
-        val packDir = pack.replace('.', '/')
+        var packageName = pack
+        if (packageName.endsWith(".")) {
+            packageName = packageName.substring(0, packageName.lastIndexOf('.'))
+        }
 
-        try {
-            val dirs = loadPackDirs(packDir)
-            for (url in dirs) {
-                when (url.protocol) {
-                    "file" -> handleFileResource(url, pack, classes)
-                    "jar" -> handleJarResource(url, classes)
-                }
+        val basePackageFilePath = packageName.replace('.', '/')
+
+        val classes = LinkedHashSet<Class<*>>()
+        val resources = contentClassLoader().getResources(basePackageFilePath)
+        for (resource in resources) {
+            val protocol = resource.protocol
+            if (protocol == "file") {
+                val filePath = URLDecoder.decode(resource.file, "UTF-8")
+                doScanPackageClassesByFile(classes, packageName, filePath)
+            } else if (protocol == "jar") {
+                doScanPackageClassesByJar(classes, resource, packageName)
             }
-        } catch (e: IOException) {
-            e.printStackTrace()
         }
 
         return classes
     }
 
-    private fun handleFileResource(url: URL, packName: String, classes: MutableSet<Class<*>>) {
-        val filePath = URLDecoder.decode(url.file, "UTF-8")
-        findAndAddClassesInPackageByFile(packName, filePath, true, classes)
-    }
-
-    private fun handleJarResource(
-        url: URL, classes: MutableSet<Class<*>>
-    ) {
-        val jar = openJarConnection(url)
-        for (entry in jar.entries()) {
-            val name = purePath(entry.name)
-            if (name.endsWith(".class") && !entry.isDirectory) {
-                val className = extractClassName(name)
-                val packName = name.replace('/', '.')
-                safeLoadClass("${packName}.$className").ifNotNull {
-                    classes.add(it)
-                }
-            }
-        }
-    }
-
-    private fun safeLoadClass(fullClassName: String): Class<*>? {
-        return runCatching { loader().loadClass(fullClassName) }.getOrNull()
-    }
-
-    private fun openJarConnection(url: URL): JarFile {
-        return (url.openConnection() as JarURLConnection).jarFile
-    }
-
-    private fun extractClassName(fullName: String): String {
-        val delimiter = fullName.lastIndexOf('/')
-        return fullName.substring(delimiter + 1, fullName.length - ".class".length)
-    }
-
-    private fun purePath(path: String): String {
-        val pre = if (path.startsWith("/")) path.substring(1) else path
-        return if (pre.endsWith("/")) pre.substring(0, pre.length) else pre
-    }
-
-    private fun findAndAddClassesInPackageByFile(
-        packageName: String,
-        packagePath: String,
-        recursive: Boolean,
-        classes: MutableSet<Class<*>>
+    private fun doScanPackageClassesByFile(
+        receiver: MutableSet<Class<*>>, packageName: String, packagePath: String
     ) {
         val dir = File(packagePath)
         if (!dir.exists() || !dir.isDirectory) {
             return
         }
 
-        val dirFiles = dir.listFiles { file ->
-            (recursive && file.isDirectory || file.name.endsWith(".class"))
+        val dirContents = dir.listFiles filter@{ file ->
+            if (file.isDirectory) {
+                return@filter true
+            }
+
+            val filename = file.name
+
+            if (!filename.endsWith(".class")) {
+                return@filter false
+            }
+
+            !filename.contains('$')
         } ?: return
 
-        for (file in dirFiles) {
+        for (file in dirContents) {
             if (file.isDirectory) {
-                findAndAddClassesInPackageByFile(
-                    "$packageName.${file.name}", file.absolutePath, recursive, classes
+                doScanPackageClassesByFile(
+                    receiver, "$packageName.${file.name}", file.absolutePath
                 )
             } else {
-                val fileName = file.name
-                val className = fileName.substring(0, fileName.length - ".class".length)
-                classes.add(loadClass(packageName, className))
+                val className = file.name.substring(0, file.name.length - 6)
+                val qualifiedName = "$packageName.$className"
+                val loadClass = runCatching {
+                    contentClassLoader().loadClass(qualifiedName)
+                }
+                if (loadClass.isSuccess) {
+                    receiver.add(loadClass.getOrThrow())
+                }
             }
         }
     }
 
-    private fun loadPackDirs(packDir: String): Enumeration<URL> {
-        return loader().getResources(packDir)
+    private fun doScanPackageClassesByJar(
+        receiver: MutableSet<Class<*>>, url: URL, basePackage: String
+    ) {
+        val basePackageFilePath = basePackage.replace('.', '/')
+        val jarFile: JarFile = (url.openConnection() as JarURLConnection).jarFile
+        for (entry in jarFile.entries()) {
+            val name = entry.name
+            if (!name.startsWith(basePackageFilePath) || entry.isDirectory) {
+                continue
+            }
+
+            if ('$' in name) {
+                continue
+            }
+
+            val className = name.replace('/', '.')
+            val qualifiedName = className.substring(0, className.length - 6)
+            val loadClass = runCatching {
+                contentClassLoader().loadClass(qualifiedName)
+            }
+            if (loadClass.isSuccess) {
+                receiver.add(loadClass.getOrThrow())
+            }
+        }
     }
 
-    private fun loadClass(packName: String, klassName: String): Class<*> {
-        return Class.forName("${packName}.$klassName")
-    }
-
-    private fun loader(): ClassLoader {
+    private fun contentClassLoader(): ClassLoader {
         return Thread.currentThread().contextClassLoader
     }
 
---
+++ vertlin-boot/src/test/kotlin/xyz/scootaloo/vertlin/test/scan/PackScanTest.kt
@@ -0,0 +1,16 @@
+package xyz.scootaloo.vertlin.test.scan
+
+import org.junit.jupiter.api.Test
+
+/**
+ * @author flutterdash@qq.com
+ * @since 2022/8/11 下午8:37
+ */
+class PackScanTest {
+
+    @Test
+    fun test() {
+
+    }
+
+}
